98187917626315bb84b149b4aa0317adb6c06be3
chethan <chethan9686@gmail.com>
2023-11-01 18:11:19 +0530

    updated migrations files

--- database/migrations/2023_10_31_111638_create_reviews_table.php
+++ database/migrations/2023_10_31_111638_create_reviews_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->unsigned()->index();
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
